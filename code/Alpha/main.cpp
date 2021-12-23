441cc5aaf1b160c2bdea36fa5321954d21856645
florent.devillechabrol <florent.devillechabrol@gmail.com>
2021-12-23 13:13:43 -0800

    Handle Resize event and Fullscreen mode.

--- code/Alpha/main.cpp
+++ code/Alpha/main.cpp
@@ -39,10 +39,17 @@ uint64_t g_FenceValue;
 uint64_t g_FrameFenceValues[g_NumFrames];
 HANDLE g_FenceEvent;
 bool g_IsInitialized = false;
+uint32_t g_ClientWidth;
+uint32_t g_ClientHeight;
+bool g_Fullscreen = false;
+HWND g_hWnd;
+RECT g_WindowRect;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void Update();
 void Render();
+void Resize(uint32_t, uint32_t);
+void SetFullscreen(bool);
 
 void RegisterWindowClass(HINSTANCE hInst, const wchar_t* pWindowClassName)
 {
@@ -113,6 +120,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 				::PostQuitMessage(0);
 				break;
 
+			case VK_F11:
+				SetFullscreen(!g_Fullscreen);
+				break;
+
 			default:
 				return ::DefWindowProcW(hWnd, message, wParam, lParam);
 				break;
@@ -122,6 +133,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	case WM_DESTROY:
 		::PostQuitMessage(0);
+		break;
+
+	case WM_SIZE:
+	{
+		RECT clientRect;
+		::GetClientRect(g_hWnd, &clientRect);
+		int width = clientRect.right - clientRect.left;
+		int height = clientRect.bottom - clientRect.top;
+
+		Resize(width, height);
+	}
 		break;
 
 	default:
@@ -458,10 +480,95 @@ void Render()
 	}
 }
 
+void Resize(uint32_t width, uint32_t height)
+{
+	if (g_ClientWidth != width || g_ClientHeight != height)
+	{
+		// Don't allow 0 size swap chain back buffers.
+		g_ClientWidth = std::max(1u, width);
+		g_ClientHeight = std::max(1u, height);
+
+		// Flush the GPU queue to make sure the swap chain's back buffers
+		// are not being referenced by an in-flight command list.
+		Flush(g_CommandQueue, g_Fence, g_FenceValue, g_FenceEvent);
+
+		for (int i = 0; i < g_NumFrames; ++i)
+		{
+			// Any references to the back buffers must be released
+			// before the swap chain can be resized.
+			g_BackBuffers[i].Reset();
+			g_FrameFenceValues[i] = g_FrameFenceValues[g_CurrentBackBufferIndex];
+		}
+
+		DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+		ThrowIfFailed(g_SwapChain->GetDesc(&swapChainDesc));
+		ThrowIfFailed(g_SwapChain->ResizeBuffers(g_NumFrames, g_ClientWidth, g_ClientHeight,
+			swapChainDesc.BufferDesc.Format, swapChainDesc.Flags));
+
+		g_CurrentBackBufferIndex = g_SwapChain->GetCurrentBackBufferIndex();
+
+		UpdateRenderTargetViews(g_Device, g_SwapChain, g_RTVDescriptorHeap);
+	}
+}
+
+void SetFullscreen(bool fullscreen)
+{
+	if (g_Fullscreen != fullscreen)
+	{
+		g_Fullscreen = fullscreen;
+
+		if (g_Fullscreen) // Switching to fullscreen.
+		{
+			// Store the current window dimensions so they can be restored 
+			// when switching out of fullscreen state.
+			::GetWindowRect(g_hWnd, &g_WindowRect);
+
+			// Set the window style to a borderless window so the client area fills
+			// the entire screen.
+			UINT windowStyle = WS_OVERLAPPEDWINDOW & ~(WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX);
+
+			::SetWindowLongW(g_hWnd, GWL_STYLE, windowStyle);
+
+			// Query the name of the nearest display device for the window.
+			// This is required to set the fullscreen dimensions of the window
+			// when using a multi-monitor setup.
+			HMONITOR hMonitor = ::MonitorFromWindow(g_hWnd, MONITOR_DEFAULTTONEAREST);
+			MONITORINFOEX monitorInfo = {};
+			monitorInfo.cbSize = sizeof(MONITORINFOEX);
+			::GetMonitorInfo(hMonitor, &monitorInfo);
+
+			::SetWindowPos(g_hWnd, HWND_TOP,
+				monitorInfo.rcMonitor.left,
+				monitorInfo.rcMonitor.top,
+				monitorInfo.rcMonitor.right - monitorInfo.rcMonitor.left,
+				monitorInfo.rcMonitor.bottom - monitorInfo.rcMonitor.top,
+				SWP_FRAMECHANGED | SWP_NOACTIVATE);
+
+			::ShowWindow(g_hWnd, SW_MAXIMIZE);
+		}
+		else
+		{
+			// Restore all the window decorators.
+			::SetWindowLong(g_hWnd, GWL_STYLE, WS_OVERLAPPEDWINDOW);
+
+			::SetWindowPos(g_hWnd, HWND_NOTOPMOST,
+				g_WindowRect.left,
+				g_WindowRect.top,
+				g_WindowRect.right - g_WindowRect.left,
+				g_WindowRect.bottom - g_WindowRect.top,
+				SWP_FRAMECHANGED | SWP_NOACTIVATE);
+
+			::ShowWindow(g_hWnd, SW_NORMAL);
+		}
+	}
+}
+
 int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstance*/, _In_ LPSTR /*lpCmdLine*/, _In_ int /*nCmdShow*/)
 {
 	int width = 1080;
 	int height = 789;
+	g_ClientWidth = width;
+	g_ClientHeight = height;
 
 	SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 
@@ -471,12 +578,12 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstanc
 
 	const wchar_t* pWindowClassName = L"DX12WindowClass";
 	RegisterWindowClass(hInstance, pWindowClassName);
-	HWND hWnd = CreateCustomWindow(pWindowClassName, hInstance, L"Alpha", width, height);
+	g_hWnd = CreateCustomWindow(pWindowClassName, hInstance, L"Alpha", width, height);
 
 	ComPtr<IDXGIAdapter4> dxgiAdapter4 = GetAdapter(false);
 	g_Device = CreateDevice(dxgiAdapter4);
 	g_CommandQueue = CreateCommandQueue(g_Device, D3D12_COMMAND_LIST_TYPE_DIRECT);
-	g_SwapChain = CreateSwapChain(hWnd, g_CommandQueue, width, height, g_NumFrames);
+	g_SwapChain = CreateSwapChain(g_hWnd, g_CommandQueue, width, height, g_NumFrames);
 	g_CurrentBackBufferIndex = g_SwapChain->GetCurrentBackBufferIndex();
 	g_RTVDescriptorHeap = CreateDescriptorHeap(g_Device, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, g_NumFrames);
 	g_RTVDescriptorSize = g_Device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
@@ -492,7 +599,7 @@ int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE /*hPrevInstanc
 
 	g_IsInitialized = true;
 
-	::ShowWindow(hWnd, SW_SHOW);
+	::ShowWindow(g_hWnd, SW_SHOW);
 
 	MSG msg = { 0 };
 	while (msg.message != WM_QUIT)
