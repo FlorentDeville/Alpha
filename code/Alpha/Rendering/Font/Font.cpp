72ffbae5b93bfa394b1f66f07935b18827e6e000
florent.devillechabrol <florent.devillechabrol@gmail.com>
2021-12-29 14:25:14 -0800

    Add font class.

---
+++ code/Alpha/Rendering/Font/Font.cpp
@@ -0,0 +1,230 @@
+/********************************************************************/
+/* © 2021 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#include "Rendering/Font/Font.h"
+
+#include <fstream>
+
+Font::Font(const std::string& name)
+    : Resource(name)
+    , m_CharList(nullptr)
+    , m_KerningsList(nullptr)
+{}
+
+Font::~Font()
+{}
+
+void Font::Init(const std::string& fntName, int windowWidth, int windowHeight)
+{
+    LoadFntFile(fntName, windowWidth, windowHeight);
+
+    std::string textureFilename = "C:\\workspace\\Alpha\\data\\fonts\\" + m_fontImage;
+    
+    Texture* pTexture = g_pTextureMgr->CreateResource(m_texture, m_fontImage);
+    pTexture->Init(textureFilename);
+}
+
+void Font::Release()
+{
+    if (m_CharList)
+    {
+        delete m_CharList;
+        m_CharList = nullptr;
+    }
+
+    if (m_KerningsList)
+    {
+        delete m_KerningsList;
+        m_KerningsList = nullptr;
+    }
+}
+
+float Font::GetKerning(char first, char second)
+{
+    for (int i = 0; i < m_numKernings; ++i)
+    {
+        if ((char)m_KerningsList[i].m_firstid == first && (char)m_KerningsList[i].m_secondid == second)
+            return m_KerningsList[i].m_amount;
+    }
+    return 0.0f;
+}
+
+FontChar* Font::GetChar(char c)
+{
+    for (int i = 0; i < m_numCharacters; ++i)
+    {
+        if (c == (char)m_CharList[i].m_id)
+            return &m_CharList[i];
+    }
+    return nullptr;
+}
+
+void Font::LoadFntFile(const std::string& fntName, int windowWidth, int windowHeight)
+{
+    std::ifstream fs;
+    fs.open(fntName);
+
+    std::string tmp;
+    size_t startpos;
+
+    // extract font name
+    fs >> tmp >> tmp; // info face="Arial"
+    startpos = tmp.find("\"") + 1;
+        m_name = tmp.substr(startpos, tmp.size() - startpos - 1);
+
+    // get font size
+    fs >> tmp; // size=73
+    startpos = tmp.find("=") + 1;
+    m_size = std::stoi(tmp.substr(startpos, tmp.size() - startpos));
+
+    // bold, italic, charset, unicode, stretchH, smooth, aa, padding, spacing
+    fs >> tmp >> tmp >> tmp >> tmp >> tmp >> tmp >> tmp; // bold=0 italic=0 charset="" unicode=0 stretchH=100 smooth=1 aa=1 
+
+    // get padding
+    fs >> tmp; // padding=5,5,5,5 
+    startpos = tmp.find("=") + 1;
+    tmp = tmp.substr(startpos, tmp.size() - startpos); // 5,5,5,5
+
+    // get up padding
+    startpos = tmp.find(",") + 1;
+    m_toppadding = std::stoi(tmp.substr(0, startpos)) / (float)windowWidth;
+
+    // get right padding
+    tmp = tmp.substr(startpos, tmp.size() - startpos);
+    startpos = tmp.find(",") + 1;
+    m_rightpadding = std::stoi(tmp.substr(0, startpos)) / (float)windowWidth;
+
+    // get down padding
+    tmp = tmp.substr(startpos, tmp.size() - startpos);
+    startpos = tmp.find(",") + 1;
+    m_bottompadding = std::stoi(tmp.substr(0, startpos)) / (float)windowWidth;
+
+    // get left padding
+    tmp = tmp.substr(startpos, tmp.size() - startpos);
+    m_leftpadding = std::stoi(tmp) / (float)windowWidth;
+
+    fs >> tmp; // spacing=0,0
+
+    fs >> tmp; // outline
+
+    // get lineheight (how much to move down for each line), and normalize (between 0.0 and 1.0 based on size of font)
+    fs >> tmp >> tmp; // common lineHeight=95
+    startpos = tmp.find("=") + 1;
+    m_lineHeight = (float)std::stoi(tmp.substr(startpos, tmp.size() - startpos)) / (float)windowHeight;
+
+    // get base height (height of all characters), and normalize (between 0.0 and 1.0 based on size of font)
+    fs >> tmp; // base=68
+    startpos = tmp.find("=") + 1;
+    m_baseHeight = (float)std::stoi(tmp.substr(startpos, tmp.size() - startpos)) / (float)windowHeight;
+
+    // get texture width
+    fs >> tmp; // scaleW=512
+    startpos = tmp.find("=") + 1;
+    m_textureWidth = std::stoi(tmp.substr(startpos, tmp.size() - startpos));
+
+    // get texture height
+    fs >> tmp; // scaleH=512
+    startpos = tmp.find("=") + 1;
+    m_textureHeight = std::stoi(tmp.substr(startpos, tmp.size() - startpos));
+
+    // get pages, packed, page id
+    fs >> tmp >> tmp; // pages=1 packed=0
+
+    fs >> tmp >> tmp >> tmp >> tmp; //alphaChnl=1 redChnl=0 greenChnl=0 blueChnl=0
+
+    //page id = 0
+    fs >> tmp >> tmp;
+
+    // get texture filename
+    std::string wtmp;
+    fs >> wtmp; // file="Arial.png"
+    startpos = wtmp.find("\"") + 1;
+        m_fontImage = wtmp.substr(startpos, wtmp.size() - startpos - 1);
+
+    // get number of characters
+    fs >> tmp >> tmp; // chars count=97
+    startpos = tmp.find("=") + 1;
+    m_numCharacters = std::stoi(tmp.substr(startpos, tmp.size() - startpos));
+
+    // initialize the character list
+    m_CharList = new FontChar[m_numCharacters];
+
+    for (int c = 0; c < m_numCharacters; ++c)
+    {
+        // get unicode id
+        fs >> tmp >> tmp; // char id=0
+        startpos = tmp.find("=") + 1;
+        m_CharList[c].m_id = std::stoi(tmp.substr(startpos, tmp.size() - startpos));
+
+        // get x
+        fs >> tmp; // x=392
+        startpos = tmp.find("=") + 1;
+        m_CharList[c].m_u = (float)std::stoi(tmp.substr(startpos, tmp.size() - startpos)) / (float)m_textureWidth;
+
+        // get y
+        fs >> tmp; // y=340
+        startpos = tmp.find("=") + 1;
+        m_CharList[c].m_v = (float)std::stoi(tmp.substr(startpos, tmp.size() - startpos)) / (float)m_textureHeight;
+
+        // get width
+        fs >> tmp; // width=47
+        startpos = tmp.find("=") + 1;
+        tmp = tmp.substr(startpos, tmp.size() - startpos);
+        m_CharList[c].m_width = (float)std::stoi(tmp) / (float)windowWidth;
+        m_CharList[c].m_twidth = (float)std::stoi(tmp) / (float)m_textureWidth;
+
+        // get height
+        fs >> tmp; // height=57
+        startpos = tmp.find("=") + 1;
+        tmp = tmp.substr(startpos, tmp.size() - startpos);
+        m_CharList[c].m_height = (float)std::stoi(tmp) / (float)windowHeight;
+        m_CharList[c].m_theight = (float)std::stoi(tmp) / (float)m_textureHeight;
+
+        // get xoffset
+        fs >> tmp; // xoffset=-6
+        startpos = tmp.find("=") + 1;
+        m_CharList[c].m_xoffset = (float)std::stoi(tmp.substr(startpos, tmp.size() - startpos)) / (float)windowWidth;
+
+        // get yoffset
+        fs >> tmp; // yoffset=16
+        startpos = tmp.find("=") + 1;
+        m_CharList[c].m_yoffset = (float)std::stoi(tmp.substr(startpos, tmp.size() - startpos)) / (float)windowHeight;
+
+        // get xadvance
+        fs >> tmp; // xadvance=65
+        startpos = tmp.find("=") + 1;
+        m_CharList[c].m_xadvance = (float)std::stoi(tmp.substr(startpos, tmp.size() - startpos)) / (float)windowWidth;
+
+        // get page
+        // get channel
+        fs >> tmp >> tmp; // page=0    chnl=0
+    }
+
+    // get number of kernings
+    fs >> tmp >> tmp; // kernings count=96
+    startpos = tmp.find("=") + 1;
+    m_numKernings = std::stoi(tmp.substr(startpos, tmp.size() - startpos));
+
+    // initialize the kernings list
+    m_KerningsList = new FontKerning[m_numKernings];
+
+    for (int k = 0; k < m_numKernings; ++k)
+    {
+        // get first character
+        fs >> tmp >> tmp; // kerning first=87
+        startpos = tmp.find("=") + 1;
+        m_KerningsList[k].m_firstid = std::stoi(tmp.substr(startpos, tmp.size() - startpos));
+
+        // get second character
+        fs >> tmp; // second=45
+        startpos = tmp.find("=") + 1;
+        m_KerningsList[k].m_secondid = std::stoi(tmp.substr(startpos, tmp.size() - startpos));
+
+        // get amount
+        fs >> tmp; // amount=-1
+        startpos = tmp.find("=") + 1;
+        int t = std::stoi(tmp.substr(startpos, tmp.size() - startpos));
+        m_KerningsList[k].m_amount = (float)t / (float)windowWidth;
+    }
+}
---
+++ code/Alpha/Rendering/Font/Font.h
@@ -0,0 +1,80 @@
+/********************************************************************/
+/* © 2021 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#pragma once
+
+#include <string>
+
+#include "Rendering/Texture/Texture.h"
+#include "Resource/Resource.h"
+
+class FontChar
+{
+public:
+
+    // the unicode id
+    int m_id;
+
+    // these need to be converted to texture coordinates 
+    // (where 0.0 is 0 and 1.0 is textureWidth of the font)
+    float m_u; // u texture coordinate
+    float m_v; // v texture coordinate
+    float m_twidth; // width of character on texture
+    float m_theight; // height of character on texture
+
+    float m_width; // width of character in screen coords
+    float m_height; // height of character in screen coords
+
+    // these need to be normalized based on size of font
+    float m_xoffset; // offset from current cursor pos to left side of character
+    float m_yoffset; // offset from top of line to top of character
+    float m_xadvance; // how far to move to right for next character
+};
+
+class FontKerning
+{
+public:
+    int m_firstid; // the first character
+    int m_secondid; // the second character
+    float m_amount; // the amount to add/subtract to second characters x
+};
+
+class Font : public Resource
+{
+public:
+    Font(const std::string& name);
+    ~Font();
+
+    void Init(const std::string& fntName, int windowWidth, int windowHeight);
+    void Release() override;
+
+    float GetKerning(char first, char second);
+    FontChar* GetChar(char c);
+
+    void LoadFntFile(const std::string& fntName, int windowWidth, int windowHeight);
+
+    //std::string m_name; // name of the font
+    std::string m_fontImage;
+    int m_size; // size of font, lineheight and baseheight will be based on this as if this is a single unit (1.0)
+    float m_lineHeight; // how far to move down to next line, will be normalized
+    float m_baseHeight; // height of all characters, will be normalized
+    int m_textureWidth; // width of the font texture
+    int m_textureHeight; // height of the font texture
+    int m_numCharacters; // number of characters in the font
+    FontChar* m_CharList; // list of characters
+    int m_numKernings; // the number of kernings
+    FontKerning* m_KerningsList; // list to hold kerning values
+    //ID3D12Resource* textureBuffer; // the font texture resource
+    TextureId m_texture;
+    D3D12_GPU_DESCRIPTOR_HANDLE m_srvHandle; // the font srv
+
+    // these are how much the character is padded in the texture. We
+    // add padding to give sampling a little space so it does not accidentally
+    // padd the surrounding characters. We will need to subtract these paddings
+    // from the actual spacing between characters to remove the gaps you would otherwise see
+    float m_leftpadding;
+    float m_toppadding;
+    float m_rightpadding;
+    float m_bottompadding;
+};
