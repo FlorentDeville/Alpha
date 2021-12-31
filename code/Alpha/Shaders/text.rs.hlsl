5b1ee1fd6d68808f22f97d41c796ed4c30e1cd95
florent.devillechabrol <florent.devillechabrol@gmail.com>
2021-12-31 06:26:30 -0800

    text shader: change sampler.

--- code/Alpha/Shaders/text.rs.hlsl
+++ code/Alpha/Shaders/text.rs.hlsl
@@ -9,7 +9,7 @@
 					"DENY_HULL_SHADER_ROOT_ACCESS |" \
 					"DENY_DOMAIN_SHADER_ROOT_ACCESS |" \
 					"DENY_GEOMETRY_SHADER_ROOT_ACCESS)," \
-					"StaticSampler(s0, visibility=SHADER_VISIBILITY_PIXEL), " \
+					"StaticSampler(s0, filter=FILTER_MIN_MAG_MIP_POINT, visibility=SHADER_VISIBILITY_PIXEL), " \
 					"DescriptorTable(SRV(t0, space=0)),"
 
 
