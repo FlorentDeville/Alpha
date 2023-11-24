3782fc912a3baf040b4c76481cd4cfe606317284
florent.devillechabrol <florent.devillechabrol@gmail.com>
2023-11-24 15:54:18 -0800

    Add BaseEvent and EventId in Widgets library.

---
+++ code/Widgets/Events/BaseEvent.h
@@ -0,0 +1,16 @@
+/********************************************************************/
+/* © 2023 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#pragma once
+
+#include "Widgets/Events/EventId.h"
+
+namespace Widgets
+{
+	class BaseEvent
+	{
+	public:
+		EventId m_id;
+	};
+}
---
+++ code/Widgets/Events/EventId.h
@@ -0,0 +1,32 @@
+/********************************************************************/
+/* © 2023 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#pragma once
+
+namespace Widgets
+{
+	enum EventId
+	{
+		kUnknown = -1,
+
+		//Mouse move event
+		kMouseMove,
+		kMouseEnter,	//The mouse entered the area of a widget
+		kMouseExit,		//The mouse exited the area of a widget
+
+		//Mouse button up/down
+		kMouseLDown,	//The mouse left button is down
+		kMouseLUp,		//The mouse left button is up
+		kMouseMDown,
+		kMouseMUp,
+		kMouseRDown,
+		kMouseRUp,
+
+		//Mouse click
+		kMouseLeftClick,
+
+		kCount
+
+	};
+}
