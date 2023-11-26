8ecc1dfaffb869813306bbc95c39acc9b86913a8
florent.devillechabrol <florent.devillechabrol@gmail.com>
2023-11-26 13:49:15 -0800

    Forgot a file in previous cl.

--- code/Widgets/Widgets/Frame.h
+++ code/Widgets/Widgets/Frame.h
@@ -6,10 +6,13 @@
 
 #include "Widgets/Container.h"
 
+#include "Core/Callbacks/CallbackList.h"
+
 #include <string>
 
 namespace Widgets
 {
+	class Icon;
 	class Label;
 	class Layout;
 	class Widget;
@@ -22,10 +25,24 @@ namespace Widgets
 
 		void AddWidget(Widget* pWidget) override;
 
+		void Close();
+
+		using OnCloseEvent = Core::CallbackList<void()>;
+
+		Core::CallbackId OnClose(const OnCloseEvent::Callback& callback);
+
 	private:		
 		Layout* m_pInternalLayout;
 		Label* m_pTitleLabel;
 		Layout* m_pTitleLayout;
 		Container* m_pInternalContainer;
+		Icon* m_pCloseIcon;
+		Icon* m_pCloseHoverIcon;
+
+		OnCloseEvent m_onClose;
+
+		void OnClick_CloseButton();
+		bool OnMouseEnter_CloseButton();
+		bool OnMouseExit_CloseButton();
 	};
 }
