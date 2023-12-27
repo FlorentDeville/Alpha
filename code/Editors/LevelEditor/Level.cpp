b93dc2d932821cbe0248b68e6cab61d7e9a68f9f
florent.devillechabrol <florent.devillechabrol@gmail.com>
2023-12-27 12:41:34 -0800

    Editors::Level : remove useless comments.

--- code/Editors/LevelEditor/Level.cpp
+++ code/Editors/LevelEditor/Level.cpp
@@ -28,31 +28,4 @@ namespace Editors
 	{
 		return m_pSceneTree;
 	}
-
-	//Core::TreeNode<Entity*>& Level::AddEntity(Entity* pNewEntity, Core::TreeNode<Entity*>& parent)
-	//{
-	//	m_entities.push_back(pNewEntity);
-	//	Core::TreeNode<Entity*>* pNewNode = parent.AddChildren(pNewEntity);
-	//	return *pNewNode;
-	//}
-
-	//void Level::DeleteEntity(Core::TreeNode<Entity*>& node)
-	//{
-	//	Entity* pEntity = node.GetContent();
-
-	//	std::vector<Entity*>::const_iterator it = std::find(m_entities.cbegin(), m_entities.cend(), pEntity);
-	//	if (it == m_entities.cend())
-	//		return;
-
-	//	m_entities.erase(it);
-	//	node.GetParent()->DeleteChild(pEntity);
-
-	//	delete pEntity;
-	//}
-
-
-	//Core::TreeNode<Entity*>& Level::GetRoot()
-	//{
-	//	return *m_sceneTree.GetRoot();
-	//}
 }
--- code/Editors/LevelEditor/Level.h
+++ code/Editors/LevelEditor/Level.h
@@ -19,19 +19,11 @@ namespace Editors
 		Level();
 		~Level();
 
-		//Core::TreeNode<Entity*>& AddEntity(Entity* pNewEntity, Core::TreeNode<Entity*>& parent);
-
-		//void DeleteEntity(Core::TreeNode<Entity*>& node);
-
-		//Core::TreeNode<Entity*>& GetRoot();
-
 		const SceneTree* GetConstSceneTree() const;
 		SceneTree* GetSceneTree();
 
 	private:
 		std::string m_name;
 		SceneTree* m_pSceneTree;
-		//std::vector<Entity*> m_entities; //list of entities. This list owns them.
-		//Core::Tree<Entity*> m_sceneTree; //tree of entities.
 	};
 }
