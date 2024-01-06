73bfbd01b223bbf99dfe0ceb362716ab9bbea449
florent.devillechabrol <florent.devillechabrol@gmail.com>
2024-01-06 15:20:20 -0800

    Update publish batch.
    Don't copy the data and raw folders.
    Copy everything in c:/alpha_publish.

--- publish.bat
+++ publish.bat
@@ -4,7 +4,7 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%b-%%a-%%c)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a-%%b)
 set DATETIME=%mydate%_%mytime%
 
-set OUTPUT=c:\workspace\Alpha\publish\Alpha_%DATETIME%
+set OUTPUT=c:\alpha_publish\Alpha_%DATETIME%
 
 set OUTPUT_DIR=%USERPROFILE%\Alpha\
 
@@ -18,9 +18,5 @@ xcopy %EXE_SRC%*.ini %OUTPUT%\bin /I /F
 
 REM copy the editor script
 xcopy %SRC_ROOT%code\scripts\*.* %OUTPUT%\scripts /I /F
-
-REM copy the data and raw
-xcopy %SRC_ROOT%data %OUTPUT%\data /I /F /S
-xcopy %SRC_ROOT%raw %OUTPUT%\raw /I /F /S
 
 echo Alpha published to %OUTPUT%
