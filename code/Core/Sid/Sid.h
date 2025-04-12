f3aa0419fe115bb45c4cca51ae2394767ec96c96
Florent Deville <florent.devillechabrol@gmail.com>
2025-04-12 13:48:54 -0700

    Add Sid class and xxh64 hashing function.

---
+++ code/Core/Sid/Sid.h
@@ -0,0 +1,38 @@
+/********************************************************************/
+/* © 2025 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#pragma once
+
+#include <string>
+
+#include "3rdparty/xxh64/xxh64.hpp"
+
+#define MAKESID(LiteralString) [&]() { constexpr Core::Sid sid = Core::MakeSid(LiteralString); return sid; } ()
+
+namespace Core
+{
+	static constexpr uint32_t PRIME_SID = 2654435761U;
+
+	// A Sid is a hashed string using xxh64. It is all constexpr but can be also used at runtime.
+	// To create a compile time generated Sid, use the macro MAKESID.
+	typedef uint64_t Sid;
+
+	static constexpr Sid MakeSid()
+	{
+		return static_cast<Sid>(-1);
+	}
+
+	static constexpr Sid MakeSid(const std::string& str)
+	{
+		return static_cast<Sid>(xxh64::hash(str.c_str(), str.size(), PRIME_SID));
+	}
+
+	template<size_t N> static constexpr Sid MakeSid(const char(&str)[N])
+	{
+		return static_cast<Sid>(xxh64::hash(str, N, PRIME_SID));
+	}
+
+	
+	static constexpr Sid INVALID_SID = MakeSid();
+}
