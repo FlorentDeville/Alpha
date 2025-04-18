b769322f0bf3d68ac44889f7d421e3dacb153fcf
Florent Deville <florent.devillechabrol@gmail.com>
2025-04-17 19:31:40 -0700

    Os : Add open file dialog.

---
+++ code/OsWin/FileDialog.h
@@ -0,0 +1,12 @@
+/********************************************************************/
+/* © 2025 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#pragma once
+
+#include <string>
+
+namespace Os
+{
+	bool OpenFileDialog(std::string& filename, const std::string& extension = "");
+}
