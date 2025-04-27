85ae03828cf6a1284073a4aeca0882cac5d74457
Florent Deville <florent.devillechabrol@gmail.com>
2025-04-26 19:13:25 -0700

    FbxImporter : apply world matrix to the vertex and convert them to meters
    Fbx uses centimeter as unit. So every value is multiplied by 100. Divide by 100 because I use the meter.

--- code/Importer/FbxImporter/FbxImporter.cpp
+++ code/Importer/FbxImporter/FbxImporter.cpp
@@ -4,10 +4,28 @@
 
 #include "Importer/FbxImporter/FbxImporter.h"
 
+#include "Core/Math/Mat44f.h"
+
 #include "ofbx.h"
 
 namespace FbxImporter
 {
+	static Core::Vec4f FromFbx(const ofbx::Vec3& fbxVec3)
+	{
+		return Core::Vec4f(fbxVec3.x, fbxVec3.y, fbxVec3.z, 1);
+	}
+
+	static Core::Mat44f FromFbx(const ofbx::DMatrix& fbxMat)
+	{
+		Core::Mat44f mat(Core::Vec4f(static_cast<float>(fbxMat.m[0]), static_cast<float>(fbxMat.m[1]), static_cast<float>(fbxMat.m[2]), static_cast<float>(fbxMat.m[3])),
+			Core::Vec4f(static_cast<float>(fbxMat.m[4]), static_cast<float>(fbxMat.m[5]), static_cast<float>(fbxMat.m[6]), static_cast<float>(fbxMat.m[7])),
+			Core::Vec4f(static_cast<float>(fbxMat.m[8]), static_cast<float>(fbxMat.m[9]), static_cast<float>(fbxMat.m[10]), static_cast<float>(fbxMat.m[11])),
+			Core::Vec4f(static_cast<float>(fbxMat.m[12]), static_cast<float>(fbxMat.m[13]), static_cast<float>(fbxMat.m[14]), static_cast<float>(fbxMat.m[15])));
+
+		return mat;
+	}
+
+
 	bool FbxImporter::Import(const std::string& fbxFilename, Systems::MeshAsset& mesh)
 	{
 		FILE* pFileHandle = nullptr;
@@ -74,6 +92,9 @@ namespace FbxImporter
 
 	bool FbxImporter::ImportMesh(const ofbx::Mesh* pMesh)
 	{
+		ofbx::DMatrix fbxWorldTx = pMesh->getGlobalTransform();
+		Core::Mat44f worldTx = FromFbx(fbxWorldTx);
+
 		const ofbx::GeometryData& data = pMesh->getGeometryData();
 
 		ofbx::Vec3Attributes positions = data.getPositions();
@@ -100,11 +121,19 @@ namespace FbxImporter
 					uint16_t internalVertexIndex = static_cast<uint16_t>(m_position.GetSize());
 
 					{
-						ofbx::Vec3 pos = positions.values[positions.indices[vertexStart]];
+						ofbx::Vec3 fbxLocalPos = positions.values[positions.indices[vertexStart]];
 						ofbx::Vec2 uv = uvs.values[uvs.indices[vertexStart]];
 						//ofbx::Vec4 color = colors.values[colors.indices[vertexStart]];
 
-						m_position.PushBack(Core::Vec4f(pos.x, pos.y, pos.z, 1));
+						
+						Core::Vec4f localPos = FromFbx(fbxLocalPos);
+						Core::Vec4f pos = localPos * worldTx;
+
+						//fbx represents everything in centimeters. Divide by 100 to get it in meters
+						pos = pos * 0.01f;
+
+						m_position.PushBack(pos);
+
 						//m_color.push_back(Core::Vec4f(color.x, color.y, color.z, color.w));
 						m_color.PushBack(Core::Vec4f(0, 0, 0, 0));
 						m_uv.PushBack(Core::Vec4f(uv.x, uv.y, 0, 0));
@@ -112,12 +141,20 @@ namespace FbxImporter
 
 					{
 						int secondVertexIndex = vertexStart + triangleIndex + 1;
-						ofbx::Vec3 pos = positions.values[positions.indices[secondVertexIndex]];
+						ofbx::Vec3 fbxLocalPos = positions.values[positions.indices[secondVertexIndex]];
 						ofbx::Vec2 uv = uvs.values[uvs.indices[secondVertexIndex]];
 						//ofbx::Vec4 color = colors.values[colors.indices[secondVertexIndex]];
 
 						size_t internalVertexIndex = m_position.GetSize();
-						m_position.PushBack(Core::Vec4f(pos.x, pos.y, pos.z, 1));
+						
+						Core::Vec4f localPos = FromFbx(fbxLocalPos);
+						Core::Vec4f pos = localPos * worldTx;
+
+						//fbx represents everything in centimeters. Divide by 100 to get it in meters
+						pos = pos * 0.01f;
+
+						m_position.PushBack(pos);
+
 						//m_color.push_back(Core::Vec4f(color.x, color.y, color.z, color.w));
 						m_color.PushBack(Core::Vec4f(0, 0, 0, 0));
 						m_uv.PushBack(Core::Vec4f(uv.x, uv.y, 0, 0));
@@ -125,12 +162,20 @@ namespace FbxImporter
 
 					{
 						int lastVertexIndex = vertexStart + triangleIndex + 2;
-						ofbx::Vec3 pos = positions.values[positions.indices[lastVertexIndex]];
+						ofbx::Vec3 fbxLocalPos = positions.values[positions.indices[lastVertexIndex]];
 						ofbx::Vec2 uv = uvs.values[uvs.indices[lastVertexIndex]];
 						//ofbx::Vec4 color = colors.values[colors.indices[lastVertexIndex]];
 
 						size_t internalVertexIndex = m_position.GetSize();
-						m_position.PushBack(Core::Vec4f(pos.x, pos.y, pos.z, 1));
+						
+						Core::Vec4f localPos = FromFbx(fbxLocalPos);
+						Core::Vec4f pos = localPos * worldTx;
+
+						//fbx represents everything in centimeters. Divide by 100 to get it in meters
+						pos = pos * 0.01f;
+
+						m_position.PushBack(pos);
+
 						//m_color.push_back(Core::Vec4f(color.x, color.y, color.z, color.w));
 						m_color.PushBack(Core::Vec4f(0, 0, 0, 0));
 						m_uv.PushBack(Core::Vec4f(uv.x, uv.y, 0, 0));
