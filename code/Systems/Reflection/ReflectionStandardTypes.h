99a980d7e5da757ed69f40f974848009fbbfb816
Florent Deville <florent.devillechabrol@gmail.com>
2025-06-28 17:11:47 -0700

    [sys] Add type char to the reflection.

--- code/Systems/Reflection/ReflectionStandardTypes.h
+++ code/Systems/Reflection/ReflectionStandardTypes.h
@@ -13,6 +13,7 @@ namespace Systems
 	DEFINE_TYPE_RESOLVER(double)
 	DEFINE_TYPE_RESOLVER(float)
 	DEFINE_TYPE_RESOLVER(bool)
+	DEFINE_TYPE_RESOLVER(char)
 	DEFINE_TYPE_RESOLVER(std::string)
 
 	// Register the standard c++ types like bool, float ,etc
