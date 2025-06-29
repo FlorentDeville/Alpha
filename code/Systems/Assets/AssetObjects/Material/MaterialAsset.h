9ff58f1ce962bb1564754d48666067f0d52a3962
Florent Deville <florent.devillechabrol@gmail.com>
2025-06-28 17:13:54 -0700

    [sys] MaterialAsset : add shader blobs.

--- code/Systems/Assets/AssetObjects/Material/MaterialAsset.h
+++ code/Systems/Assets/AssetObjects/Material/MaterialAsset.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include "Core/Collections/Array.h"
 #include "Systems/Objects/AssetObject.h"
 
 namespace Systems
@@ -19,11 +20,18 @@ namespace Systems
 		std::string m_sourceFileVS;
 		std::string m_sourceFileRS;
 
+		Core::Array<char> m_psBlob;
+		Core::Array<char> m_vsBlob;
+		Core::Array<char> m_rsBlob;
+
 		START_REFLECTION(Systems::MaterialAsset_v1)
 			ADD_BASETYPE(Systems::AssetObject)
 			ADD_FIELD(m_sourceFilePS)
 			ADD_FIELD(m_sourceFileVS)
 			ADD_FIELD(m_sourceFileRS)
+			ADD_FIELD_ATTR(m_psBlob, DoNotEdit)
+			ADD_FIELD_ATTR(m_vsBlob, DoNotEdit)
+			ADD_FIELD_ATTR(m_rsBlob, DoNotEdit)
 		END_REFLECTION()
 	};
 
