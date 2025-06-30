6cce3d31199d63059388c4b9c8db638db03cf022
Florent Deville <florent.devillechabrol@gmail.com>
2025-06-29 20:23:18 -0700

    [sys] MaterialAsset : add PostLoad to generate the rendering objects from the asset.

--- code/Systems/Assets/AssetObjects/Material/MaterialAsset.cpp
+++ code/Systems/Assets/AssetObjects/Material/MaterialAsset.cpp
@@ -4,13 +4,28 @@
 
 #include "Systems/Assets/AssetObjects/Material/MaterialAsset.h"
 
+#include "Rendering/PipelineState/PipelineState.h"
+#include "Rendering/RootSignature/RootSignature.h"
+#include "Rendering/Shaders/Shader.h"
+
 namespace Systems
 {
 	MaterialAsset_v1::MaterialAsset_v1()
 		: m_sourceFilePS()
 		, m_sourceFileRS()
 		, m_sourceFileVS()
+		, m_pVs(nullptr)
+		, m_pPs(nullptr)
+		, m_pRs(nullptr)
 	{ }
+
+	MaterialAsset_v1::~MaterialAsset_v1()
+	{
+		delete m_pVs;
+		delete m_pPs;
+		delete m_pRs;
+		delete m_pPipelineState;
+	}
 
 	const std::string& MaterialAsset_v1::GetSourceFilePs() const
 	{
@@ -35,5 +50,59 @@ namespace Systems
 	Core::Array<char>& MaterialAsset_v1::GetRsBlob()
 	{
 		return m_rsBlob;
+	}
+
+	void MaterialAsset_v1::PostLoad()
+	{
+		UpdateRenderingObjects();
+	}
+
+	void MaterialAsset_v1::UpdateRenderingObjects()
+	{
+		bool validVertexShader = false;
+		if (m_vsBlob.GetSize() != 0)
+		{
+			delete m_pVs;
+			m_pVs = new Rendering::Shader();
+			bool res = m_pVs->LoadFromMemory(m_vsBlob);
+			if (!res)
+				return;
+
+			validVertexShader = true;
+		}
+
+		bool validPixelShader = false;
+		if (m_psBlob.GetSize() != 0)
+		{
+			delete m_pPs;
+			m_pPs = new Rendering::Shader();
+			bool res = m_pPs->LoadFromMemory(m_psBlob);
+			if (!res)
+				return;
+
+			validPixelShader = true;
+		}
+
+		bool validRootSignature = false;
+		if (m_rsBlob.GetSize() != 0)
+		{
+			delete m_pRs;
+			m_pRs = new Rendering::RootSignature();
+			bool res = m_pRs->LoadFromMemory(m_rsBlob);
+			if (!res)
+				return;
+
+			validRootSignature = true;
+		}
+
+		bool createPipelineState = validPixelShader && validVertexShader && validRootSignature;
+		if (createPipelineState)
+		{
+			if (m_pPipelineState)
+				delete m_pPipelineState;
+
+			m_pPipelineState = new Rendering::PipelineState();
+			m_pPipelineState->Init_Generic(*m_pRs, *m_pVs, *m_pPs);
+		}
 	}
 }
--- code/Systems/Assets/AssetObjects/Material/MaterialAsset.h
+++ code/Systems/Assets/AssetObjects/Material/MaterialAsset.h
@@ -7,6 +7,13 @@
 #include "Core/Collections/Array.h"
 #include "Systems/Objects/AssetObject.h"
 
+namespace Rendering
+{
+	class PipelineState;
+	class RootSignature;
+	class Shader;
+}
+
 namespace Systems
 {
 	ENABLE_REFLECTION_WITH_NS(Systems, MaterialAsset_v1)
@@ -14,6 +21,7 @@ namespace Systems
 	{
 	public:
 		MaterialAsset_v1();
+		~MaterialAsset_v1();
 
 		const std::string& GetSourceFilePs() const;
 		const std::string& GetSourceFileVs() const;
@@ -21,6 +29,10 @@ namespace Systems
 		Core::Array<char>& GetPsBlob();
 		Core::Array<char>& GetVsBlob();
 		Core::Array<char>& GetRsBlob();
+
+		void PostLoad() override;
+
+		void UpdateRenderingObjects();
 
 	private:
 		std::string m_sourceFilePS;
@@ -30,6 +42,12 @@ namespace Systems
 		Core::Array<char> m_psBlob;
 		Core::Array<char> m_vsBlob;
 		Core::Array<char> m_rsBlob;
+
+		//No reflected variables
+		Rendering::Shader* m_pVs;
+		Rendering::Shader* m_pPs;
+		Rendering::RootSignature* m_pRs;
+		Rendering::PipelineState* m_pPipelineState;
 
 		START_REFLECTION(Systems::MaterialAsset_v1)
 			ADD_BASETYPE(Systems::AssetObject)
