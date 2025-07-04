ea95b915e28865f1ad48cf1d231d870ed1ec8bb1
Florent Deville <florent.devillechabrol@gmail.com>
2025-07-04 13:12:35 -0700

    [gfx] Add c++ definition of lights cbuffer.

---
+++ code/Rendering/ConstantBuffer/LightsCBuffer.cpp
@@ -0,0 +1,29 @@
+/********************************************************************/
+/* © 2025 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#include "Rendering/ConstantBuffer/LightsCBuffer.h"
+
+namespace Rendering
+{
+	Light::Light()
+		: m_position()
+		, m_direction()
+		, m_coneAngle(0)
+		, m_type(LightType::Directional)
+	{ }
+
+	Light Light::MakeDirectionalLight(const DirectX::XMFLOAT3& direction)
+	{
+		Light newLight;
+		newLight.m_direction = direction;
+		newLight.m_type = LightType::Directional;
+		return newLight;
+	}
+
+	LightsCBuffer::LightsCBuffer(const Light& light)
+	{
+		m_numLight = 1;
+		m_lightArray[0] = light;
+	}
+}
---
+++ code/Rendering/ConstantBuffer/LightsCBuffer.h
@@ -0,0 +1,48 @@
+/********************************************************************/
+/* © 2025 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#pragma once
+
+#include <DirectXMath.h>
+
+namespace Rendering
+{
+	enum LightType : int
+	{
+		Directional = 0,
+		Point,
+		Spot,
+
+		Count
+	};
+
+	class Light
+	{
+	public:
+		Light();
+
+		static Light MakeDirectionalLight(const DirectX::XMFLOAT3& direction);
+
+	private:
+		DirectX::XMFLOAT3 m_position;
+		char m_padding0[4];
+		DirectX::XMFLOAT3 m_direction;
+		char m_padding1[4];
+		float m_coneAngle;
+		LightType m_type;
+	};
+
+	// This is the structure representing the lights const buffer. It needs to match the structure passed to shaders
+	// and defined in includes\CBufferLights.hlsl.
+	struct LightsCBuffer
+	{
+	public:
+		LightsCBuffer(const Light& light);
+
+	private:
+		int m_numLight;
+		char m_padding[12];
+		Light m_lightArray[8];
+	};
+}
