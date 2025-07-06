b63405b1836920d60fcd5117752b048f10c1b112
Florent Deville <florent.devillechabrol@gmail.com>
2025-07-06 15:10:41 -0700

    [sys] MaterialInstanceAsset : add implementation of constructor.

---
+++ code/Systems/Assets/AssetObjects/Material/MaterialInstanceAsset.cpp
@@ -0,0 +1,16 @@
+/********************************************************************/
+/* © 2025 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#include "Systems/Assets/AssetObjects/Material/MaterialInstanceAsset.h"
+
+namespace Systems
+{
+	MaterialInstanceAsset_v1::MaterialInstanceAsset_v1()
+		: AssetObject()
+	{ }
+
+	MaterialInstanceAsset_v1::~MaterialInstanceAsset_v1()
+	{ }
+
+}
