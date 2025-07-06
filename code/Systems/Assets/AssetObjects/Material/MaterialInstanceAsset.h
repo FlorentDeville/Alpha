fd5fee594233fe99ea961f7c7d91f6f61c24826e
Florent Deville <florent.devillechabrol@gmail.com>
2025-07-06 14:33:22 -0700

    [sys] Add MaterialInstance asset.

---
+++ code/Systems/Assets/AssetObjects/Material/MaterialInstanceAsset.h
@@ -0,0 +1,33 @@
+/********************************************************************/
+/* © 2025 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#pragma once
+
+#include "Core/Collections/Array.h"
+
+#include "Systems/Assets/AssetObjects/Material/MaterialParameterDescription.h"
+#include "Systems/Assets/NewAssetId.h"
+#include "Systems/Objects/AssetObject.h"
+
+namespace Systems
+{
+	ENABLE_REFLECTION_WITH_NS(Systems, MaterialInstanceAsset_v1)
+	class MaterialInstanceAsset_v1 : public AssetObject
+	{
+	public:
+		MaterialInstanceAsset_v1();
+		~MaterialInstanceAsset_v1();
+
+
+	private:
+		NewAssetId m_material;
+		Core::Array<MaterialParameterDescription> m_perMaterialParameters;
+
+		START_REFLECTION(Systems::MaterialInstanceAsset_v1)
+			ADD_BASETYPE(Systems::AssetObject)
+			ADD_FIELD(m_material)
+			ADD_FIELD(m_perMaterialParameters)
+		END_REFLECTION()
+	};
+}
