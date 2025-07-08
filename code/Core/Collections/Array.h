d4f87ba0464c1d63ab9efb7237a8e1f2b5d3f1e9
Florent Deville <florent.devillechabrol@gmail.com>
2025-07-07 17:10:44 -0700

    [cor] Array : fix pointer arithmetic in Resize when setting the default value.

--- code/Core/Collections/Array.h
+++ code/Core/Collections/Array.h
@@ -207,8 +207,11 @@ namespace Core
 
 		if (m_size > oldSize)
 		{
-			for(uint32_t ii = oldSize; ii < m_size; ++ii)
-				memcpy(m_pStart + (sizeof(T) * ii), &value, sizeof(value));
+			for (uint32_t ii = oldSize; ii < m_size; ++ii)
+			{
+				// m_pStart is a T*, so I can just increase the pointer
+				memcpy(m_pStart + ii, &value, sizeof(value));
+			}
 		}
 	}
 
