3c6a62e292ca5e22addbb30810c0c0c4c490bdd7
Florent Deville <florent.devillechabrol@gmail.com>
2025-07-14 18:55:48 -0700

    Add new asset type Level.

---
+++ code/Systems/Assets/AssetObjects/Level/LevelAsset.h
@@ -0,0 +1,12 @@
+/********************************************************************/
+/* © 2025 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#pragma once
+
+#include "Systems/Assets/AssetObjects/Level/LevelAsset_v1.h"
+
+namespace Systems
+{
+	using LevelAsset = LevelAsset_v1;
+}
---
+++ code/Systems/Assets/AssetObjects/Level/LevelAsset_v1.h
@@ -0,0 +1,33 @@
+/********************************************************************/
+/* © 2025 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************/
+
+#pragma once
+
+#include "Core/Sid/Sid.h"
+
+#include "Systems/Objects/AssetObject.h"
+#include "Systems/Objects/GameObject.h"
+
+namespace Systems
+{
+	ENABLE_REFLECTION_WITH_NS(Systems, LevelAsset_v1)
+	class LevelAsset_v1 : public AssetObject
+	{
+	public:
+		LevelAsset_v1();
+		~LevelAsset_v1();
+
+		static const std::string& GetAssetTypeName();
+		static Core::Sid GetAssetTypeNameSid();
+
+	private:
+
+		Core::Array<GameObject*> m_gameObjectsArray;
+
+		START_REFLECTION(Systems::LevelAsset_v1)
+			ADD_BASETYPE(Systems::Object)
+			ADD_FIELD(m_gameObjectsArray)
+		END_REFLECTION()
+	};
+}
