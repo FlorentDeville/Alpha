1a76ee393accfc60f0bfa91479a73ea8c0cecab5
Florent Deville <florent.devillechabrol@gmail.com>
2025-08-02 10:43:19 -0700

    [edt] Update the PropertyGridItem class to make it the base class of property items.
    Each type supported by the property grid can inherit from PropertyGridItem and creates its own widgets.
    It also allows the property grid populator to update the value displayed in the widget when the underlying value is changed in the object.

--- code/Editors/Widgets/PropertyGrid/PropertyGridItem.h
+++ code/Editors/Widgets/PropertyGrid/PropertyGridItem.h
@@ -6,6 +6,12 @@
 
 #include <string>
 
+namespace Systems
+{
+	class FieldDescriptor;
+	class Object;
+}
+
 namespace Widgets
 {
 	class Widget;
@@ -20,13 +26,34 @@ namespace Editors
 		PropertyGridItem();
 		PropertyGridItem(const std::string& name, Widgets::Widget* pEditingWidget);
 		PropertyGridItem(Widgets::Widget* pNameWidget, Widgets::Widget* pEditingWidget);
+		PropertyGridItem(Systems::Object* pObj, Systems::FieldDescriptor* pField, uint32_t index);
 
 		Widgets::Widget* GetNameWidget() const;
 		Widgets::Widget* GetEditingWidget() const;
 
-	private:
-		//The widget is not owned by the property
+		// Create the widgets rendered by the PropertyGridWidget
+		virtual void CreateWidgets();
+
+		// Update the value displayed in the widget
+		virtual void UpdateValue();
+
+		// Check if this item corresponds to a certain field for an given object
+		bool IsField(const Systems::Object* pObj, const Systems::FieldDescriptor* pField, uint32_t index) const;
+
+	protected:
+
+		//The widgets are not owned by the item.
 		Widgets::Widget* m_pEditingWidget;
 		Widgets::Widget* m_pNameWidget;
+
+		//Object, field and optional index this item represents.
+		Systems::Object* m_pObj;
+		Systems::FieldDescriptor* m_pField;
+		uint32_t m_index;
+
+		Widgets::Widget* CreateDefaultItemLabel();
+
+		Widgets::Widget* CreatePodItemLabel();
+		Widgets::Widget* CreateArrayItemLabel();
 	};
 }
