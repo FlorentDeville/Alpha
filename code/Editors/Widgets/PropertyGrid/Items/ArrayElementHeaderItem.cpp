c31771343ce105a526a66ce1baa078db5de1813a
Florent Deville <florent.devillechabrol@gmail.com>
2025-08-10 00:03:26 -0700

    [edt] ArrayElementHeaderItem : reuse function CreateDefaultItemLabel.

--- code/Editors/Widgets/PropertyGrid/Items/ArrayElementHeaderItem.cpp
+++ code/Editors/Widgets/PropertyGrid/Items/ArrayElementHeaderItem.cpp
@@ -29,63 +29,6 @@ namespace Editors
 	void ArrayElementHeaderItem::CreateWidgets()
 	{
 		m_pEditingWidget = nullptr;
-
-		Widgets::Layout* pNameLayout = new Widgets::Layout(Widgets::Layout::Horizontal, Widgets::Widget::FIT);
-		pNameLayout->GetDefaultStyle().SetBackgroundColor(Widgets::Color(0, 0, 0, 0));
-		pNameLayout->GetHoverStyle().SetBackgroundColor(Widgets::Color(0, 0, 0, 0));
-		pNameLayout->SetSpace(DirectX::XMINT2(5, 0));
-
-		const Widgets::WidgetMgr& widgetMgr = Widgets::WidgetMgr::Get();
-		Rendering::TextureId deleteTextureId = widgetMgr.GetIconTextureId(Widgets::IconId::kIconClose);
-		Widgets::Icon* pDeleteIcon = new Widgets::Icon(deleteTextureId);
-
-		Widgets::Button* pDeleteButton = new Widgets::Button(12, 12, 0, 0);
-		pDeleteButton->AddWidget(pDeleteIcon);
-		pDeleteButton->SetPositionStyle(Widgets::Widget::HPOSITION_STYLE::NONE, Widgets::Widget::VPOSITION_STYLE::MIDDLE);
-		pDeleteButton->OnClick([this]()
-			{
-				Editors::OkCancelDialog* pDialog = new Editors::OkCancelDialog("Delete element", "Are you sure you want to delete this element from the array ?");
-				pDialog->OnOk([this]()
-					{
-						ObjectWatcher::Get().ModifyField(m_pObj, m_pField, ObjectWatcher::REMOVE_ELEMENT, m_index, nullptr);
-					});
-
-				pDialog->Open();
-			});
-
-		pNameLayout->AddWidget(pDeleteButton);
-
-		const int BUFFER_SIZE = 64;
-		char buffer[BUFFER_SIZE] = { '\0' };
-
-		const Systems::TypeDescriptor* pArrayType = m_pField->GetType();
-		const Systems::TypeDescriptor* pArrayElementType = pArrayType->GetElementType();
-
-		bool elementIsObject = pArrayElementType->IsObject();
-		if (elementIsObject)
-		{
-			if (pArrayType->IsElementPointer())
-			{
-				const Core::BaseArray* pArray = m_pField->GetDataPtr<const Core::BaseArray>(m_pObj);
-				const void* pElement = pArray->GetConstElement(m_index);
-				const Systems::Object* const* pObject = reinterpret_cast<const Systems::Object* const*>(pElement);
-				const Systems::TypeDescriptor* pObjectType = (*pObject)->GetTypeDescriptor();
-				sprintf_s(buffer, "[%d] %s", m_index, pObjectType->GetName().c_str());
-			}
-			else
-			{
-				sprintf_s(buffer, "[%d] %s", m_index, pArrayElementType->GetName().c_str());
-			}
-		}
-		else
-		{
-			sprintf_s(buffer, "[%d]", m_index);
-		}
-
-		Widgets::Label* pNameLabel = new Widgets::Label(buffer);
-		pNameLabel->SetSizeStyle(Widgets::Widget::HSIZE_FIT | Widgets::Widget::VSIZE_DEFAULT);
-		pNameLayout->AddWidget(pNameLabel);
-
-		m_pNameWidget = pNameLayout;
+		m_pNameWidget = CreateDefaultItemLabel();
 	}
 }
