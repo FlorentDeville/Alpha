0c4c836e514643106a5093296452c29dedf1d645
Florent Deville <florent.devillechabrol@gmail.com>
2026-02-17 09:01:55 -0800

    [edt] Factorize shader code for light structures.

--- code/Editors/MaterialEditor/ShaderAutoGen.cpp
+++ code/Editors/MaterialEditor/ShaderAutoGen.cpp
@@ -29,6 +29,7 @@ namespace Editors
 		WriteCBufferPerObjectHeaderFile(cleanFolder);
 		WriteCBufferLightsHeaderFile(cleanFolder);
 		WriteBaseTypesHeaderFile(cleanFolder);
+		WriteLightTypesHeaderFile(cleanFolder);
 	}
 
 	void ShaderAutoGen::WriteCBufferPerFrameHeaderFile(const std::string& folder)
@@ -111,30 +112,10 @@ namespace Editors
 		fileContent += "#ifndef CBUFFER_LIGHTS_HLSL" + NEW_LINE;
 		fileContent += "#define CBUFFER_LIGHTS_HLSL" + NEW_LINE + NEW_LINE;
 
+		fileContent += "#include \"LightTypes.hlsl\"" + NEW_LINE + NEW_LINE;
+
 		//set the matrix as row major
 		fileContent += "#pragma pack_matrix(row_major)" + NEW_LINE + NEW_LINE;
-
-		//define constant
-		fileContent += "#define MAX_LIGHT_COUNT 8 \n\n"
-			"#define DIRECTIONAL_LIGHT 0\n"
-			"#define POINT_LIGHT 1\n"
-			"#define SPOT_LIGHT 2\n" + NEW_LINE;
-
-		//write the light structure
-		fileContent += "struct Light\n"
-			"{\n"
-			"\tfloat3 position;\n"
-			"\tfloat outerCutOff;\n"
-			"\tfloat3 direction;\n\n"
-			"\tfloat3 ambient;\n"
-			"\tfloat3 diffuse;\n"
-			"\tfloat3 specular;\n\n"
-			"\tfloat constantAttenuation;\n"
-			"\tfloat linearAttenuation;\n"
-			"\tfloat quadraticAttenuation;\n\n"
-			"\tfloat cutOff;\n\n"
-			"\tint type; //0 : directional, 1 : point, 2 : spot\n"
-			"};\n" + NEW_LINE;
 
 		//write the cbuffer
 		fileContent += "cbuffer CBufferLights\n"
@@ -176,6 +157,53 @@ namespace Editors
 		fileContent += "#endif // ifndef BASE_TYPE_HLSL" + NEW_LINE;
 
 		std::string filename = folder + "BaseTypes.hlsl";
+
+		FILE* pFile = nullptr;
+		fopen_s(&pFile, filename.c_str(), "w");
+		if (!pFile)
+			return;
+
+		size_t byteWritten = fwrite(fileContent.c_str(), sizeof(char), fileContent.size(), pFile);
+
+		fclose(pFile);
+	}
+
+	void ShaderAutoGen::WriteLightTypesHeaderFile(const std::string& folder)
+	{
+		std::string fileContent;
+
+		WriteCopyright(fileContent);
+
+		//Write pragma once
+		fileContent += "#ifndef LIGHT_TYPES_HLSL" + NEW_LINE;
+		fileContent += "#define LIGHT_TYPES_HLSL" + NEW_LINE + NEW_LINE;
+
+		
+		fileContent += "#define MAX_LIGHT_COUNT 8" + NEW_LINE + NEW_LINE;
+
+		fileContent += "#define DIRECTIONAL_LIGHT 0" + NEW_LINE;
+		fileContent += "#define POINT_LIGHT 1" + NEW_LINE;
+		fileContent += "#define SPOT_LIGHT 2" + NEW_LINE;
+
+		//write the light structure
+		fileContent += "struct Light\n"
+			"{\n"
+			"\tfloat3 position;\n"
+			"\tfloat outerCutOff;\n"
+			"\tfloat3 direction;\n\n"
+			"\tfloat3 ambient;\n"
+			"\tfloat3 diffuse;\n"
+			"\tfloat3 specular;\n\n"
+			"\tfloat constantAttenuation;\n"
+			"\tfloat linearAttenuation;\n"
+			"\tfloat quadraticAttenuation;\n\n"
+			"\tfloat cutOff;\n\n"
+			"\tint type; //0 : directional, 1 : point, 2 : spot\n"
+			"};\n" + NEW_LINE;
+
+		fileContent += "#endif // ifndef LIGHT_TYPES_HLSL" + NEW_LINE;
+
+		std::string filename = folder + "LightTypes.hlsl";
 
 		FILE* pFile = nullptr;
 		fopen_s(&pFile, filename.c_str(), "w");
