066e6ba233531f473d4a842e3de3e3a19d4efb8a
Florent Deville <florent.devillechabrol@gmail.com>
2026-02-20 13:16:37 -0800

    [edt] New class TextureEditorModule.

--- code/Editors/TextureEditor/TextureEditor.cpp
+++ code/Editors/TextureEditor/TextureEditor.cpp
@@ -4,6 +4,14 @@
 
 #include "Editors/TextureEditor/TextureEditor.h"
 
+#include "Editors/TextureEditor/TextureEditorModule.h"
+
+#include "OsWin/FileDialog.h"
+
+#include "Widgets/Menu.h"
+#include "Widgets/MenuBar.h"
+#include "Widgets/MenuItem.h"
+
 namespace Editors
 {
 	TextureEditor::TextureEditor()
@@ -16,5 +24,23 @@ namespace Editors
 	void TextureEditor::CreateEditor(Widgets::Widget* pParent)
 	{
 		CreateDefaultWidgets(pParent, "Texture");
+
+		//create the file menu
+		{
+			Widgets::Menu* pFileMenu = m_pMenuBar->AddMenu("File");
+
+			Widgets::MenuItem* pNewItem = pFileMenu->AddMenuItem("Import...");
+			pNewItem->OnClick([this]() { OnClick_File_Import(); });
+		}
+	}
+
+	void TextureEditor::OnClick_File_Import()
+	{
+		std::string filename;
+		bool res = Os::OpenFileDialog(filename);
+		if (!res)
+			return;
+
+		TextureEditorModule::Get().Import(filename);
 	}
 }
