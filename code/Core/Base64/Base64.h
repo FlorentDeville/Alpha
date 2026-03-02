862ed914b99ab7ae7c89d162cecee347ea40e03e
Florent Deville <florent.devillechabrol@gmail.com>
2026-03-02 15:02:41 -0800

    [sys] Deserialize blob from Base64.

--- code/Core/Base64/Base64.h
+++ code/Core/Base64/Base64.h
@@ -13,7 +13,9 @@ namespace Core
 	public:
 		//Compute the size of the encoded string from the size of the decoded data size.
 		static uint32_t EncodedSize(uint32_t decodedDataSize);
+		static uint32_t DecodedSize(const char* pData, uint32_t encodedDataSize);
 
 		static void Encode(const uint8_t* pData, uint32_t size, char* pBuffer);
+		static void Decode(const char* pData, uint32_t dataSize, uint8_t* pBuffer, uint32_t bufferSize);
 	};
 }
