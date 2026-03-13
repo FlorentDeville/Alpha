90e763567d884e82328042676ba481ebc8c97f29
Florent Deville <florent.devillechabrol@gmail.com>
2026-03-12 17:21:04 -0700

    [edt] Utils::FToA : truncate and round the value.

--- code/Editors/Widgets/Utils.cpp
+++ code/Editors/Widgets/Utils.cpp
@@ -11,18 +11,8 @@ namespace Editors
 {
 	void Utils::FToA(float value, char* pBuffer, uint32_t bufferSize)
 	{
-		float absValue = std::abs(value);
+		float roundedValue = static_cast<int32_t>(std::round(value * 1000)) * 0.001f;
 
-		int32_t integerPart = static_cast<int32_t>(std::trunc(absValue));
-		int32_t decimalPart = static_cast<int32_t>(std::trunc((absValue - integerPart) * 1000));
-
-		const char* pPositive = "";
-		const char* pNegative = "-";
-		const char* pSign = value < 0 ? pNegative : pPositive;
-
-		if (decimalPart != 0)
-			snprintf(pBuffer, bufferSize, "%s%d.%d", pSign, integerPart, decimalPart);
-		else
-			snprintf(pBuffer, bufferSize, "%s%d", pSign, integerPart);
+		snprintf(pBuffer, bufferSize, "%g", roundedValue);
 	}
 }
