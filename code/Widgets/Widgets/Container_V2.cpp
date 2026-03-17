20a3174afa76af07eb9dc67956c334498cfae44a
Florent Deville <florent.devillechabrol@gmail.com>
2026-03-17 16:10:07 -0700

    [wdg] New Container_V2 class. It's a container with scrolling.

---
+++ code/Widgets/Widgets/Container_V2.cpp
@@ -0,0 +1,45 @@
+/********************************************************************************/
+/* Copyright (C) 2026 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************************/
+
+#include "Widgets/Widgets/Container_V2.h"
+
+namespace Widgets
+{
+	Container_V2::Container_V2()
+		: Widget()
+	{
+		m_virtualSize.x = UINT32_MAX;
+		m_virtualSize.y = UINT32_MAX;
+
+		//debug
+		m_showBorder = true;
+		m_borderWidth = 1;
+		m_borderColor = Color(255, 255, 255);
+	}
+
+	Container_V2::~Container_V2()
+	{ }
+
+	void Container_V2::Draw(const Core::Float2& windowSize, const D3D12_RECT& scissor)
+	{
+		D3D12_RECT innerRect;
+		innerRect.left = m_absPos.x;
+		innerRect.right = m_absPos.x + m_size.x;
+		innerRect.top = m_absPos.y;
+		innerRect.bottom = m_absPos.y + m_size.y;
+		Parent::Draw(windowSize, innerRect);
+	}
+
+	bool Container_V2::Handle(const BaseEvent& ev)
+	{
+		return Parent::Handle(ev);
+	}
+
+	void Container_V2::ResizeChildren()
+	{
+		//Parent::ResizeChildren();
+		for (Widget* pChild : m_children)
+			pChild->Resize(m_absPos, m_virtualSize);
+	}
+}
---
+++ code/Widgets/Widgets/Container_V2.h
@@ -0,0 +1,26 @@
+/********************************************************************************/
+/* Copyright (C) 2026 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************************/
+
+#pragma once
+
+#include "Widgets/Widget.h"
+
+namespace Widgets
+{
+	class Container_V2 : public Widget
+	{
+	public:
+		using Parent = Widget;
+
+		Container_V2();
+		~Container_V2();
+
+		void Draw(const Core::Float2& windowSize, const D3D12_RECT& scissor) override;
+		bool Handle(const BaseEvent& ev) override;
+		void ResizeChildren() override;
+
+	private:
+		Core::UInt2 m_virtualSize; //size propagated to the children
+	};
+}
