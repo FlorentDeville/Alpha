fde0f409687f63df62a63275832e6bf78115da8a
Florent Deville <florent.devillechabrol@gmail.com>
2026-03-24 15:47:07 -0700

    [alpha] GameMgr : New functions to load/unload levels. Implment basic Update function.

--- code/Alpha/GameMgr.cpp
+++ code/Alpha/GameMgr.cpp
@@ -1,8 +1,12 @@
-/********************************************************************/
-/* © 2022 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
-/********************************************************************/
+/********************************************************************************/
+/* Copyright (C) 2022 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************************/
 
 #include "GameMgr.h"
+
+#include "Systems/Assets/AssetObjects/AssetUtil.h"
+#include "Systems/Assets/AssetObjects/Level/LevelAsset.h"
+#include "Systems/Objects/GameObject.h"
 
 GameMgr::GameMgr()
 { }
@@ -17,7 +21,86 @@ void GameMgr::Release()
 { }
 
 void GameMgr::Update()
-{ }
+{
+	ExecuteLoadingRequests();
+	ExecuteUnloadingRequests();
+
+	for (Systems::LevelAsset* pLevel : m_loadedLevels)
+	{
+		//going through the list of game object of levels will cause problems when game objects
+		//are created at runtime.
+		//I should make an internal array of all game object pointers in the GameMgr.
+		Core::Array<Systems::GameObject*>& gameObjects = pLevel->GetGameObjectsArray();
+		for (Systems::GameObject* pGo : gameObjects)
+		{
+			pGo->Update();
+		}
+	}
+}
 
 void GameMgr::Render()
-{ }
+{
+
+}
+
+void GameMgr::RequestLoadingLevel(Systems::NewAssetId levelId)
+{
+	m_loadingRequest.PushBack(levelId);
+}
+
+void GameMgr::RequestUnloadingLevel(Systems::NewAssetId levelId)
+{
+	m_unloadingRequest.PushBack(levelId);
+}
+
+void GameMgr::RequestUnloadingAllLevels()
+{
+	m_unloadingRequest = m_loadedLevelsIds;
+}
+
+bool GameMgr::IsLevelAlreadyLoaded(Systems::NewAssetId id) const
+{
+	for (const Systems::NewAssetId loadedId : m_loadedLevelsIds)
+	{
+		if (id == loadedId)
+			return true;
+	}
+
+	return false;
+}
+
+void GameMgr::ExecuteLoadingRequests()
+{
+	for (const Systems::NewAssetId id : m_loadingRequest)
+	{
+		if (IsLevelAlreadyLoaded(id))
+			continue;
+
+		Systems::LevelAsset* pLevel = Systems::AssetUtil::LoadAsset<Systems::LevelAsset>(id);
+		if (!pLevel)
+			return; //doesn't exist
+
+		m_loadedLevelsIds.PushBack(id);
+		m_loadedLevels.PushBack(pLevel);
+	}
+
+	m_loadingRequest.Clear();
+}
+
+void GameMgr::ExecuteUnloadingRequests()
+{
+	for (const Systems::NewAssetId id : m_unloadingRequest)
+	{
+		if (!IsLevelAlreadyLoaded(id))
+			continue;
+
+		Systems::LevelAsset* pLevel = Systems::AssetUtil::GetAsset<Systems::LevelAsset>(id);
+		if (!pLevel)
+			return; //doesn't exist
+
+		m_loadedLevelsIds.Erase(id);
+		m_loadedLevels.Erase(pLevel);
+	}
+
+	m_unloadingRequest.Clear();
+}
--- code/Alpha/GameMgr.h
+++ code/Alpha/GameMgr.h
@@ -1,10 +1,18 @@
-/********************************************************************/
-/* © 2022 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
-/********************************************************************/
+/********************************************************************************/
+/* Copyright (C) 2022 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************************/
 
 #pragma once
 
+#include "Core/Collections/Array.h"
 #include "Core/Singleton.h"
+
+#include "Systems/Assets/NewAssetId.h"
+
+namespace Systems
+{
+	class LevelAsset;
+}
 
 class GameMgr : public Core::Singleton<GameMgr>
 {
@@ -17,4 +25,21 @@ public:
 
 	void Update();
 	void Render();
+
+	void RequestLoadingLevel(Systems::NewAssetId levelId);
+	void RequestUnloadingLevel(Systems::NewAssetId levelId);
+
+	void RequestUnloadingAllLevels();
+
+private:
+	Core::Array<Systems::NewAssetId> m_loadedLevelsIds;	//currently loaded level
+	Core::Array<Systems::LevelAsset*> m_loadedLevels;		//pointers to the currently loaded levels.
+
+	Core::Array<Systems::NewAssetId> m_loadingRequest;
+	Core::Array<Systems::NewAssetId> m_unloadingRequest;
+
+	bool IsLevelAlreadyLoaded(Systems::NewAssetId id) const;
+
+	void ExecuteLoadingRequests();
+	void ExecuteUnloadingRequests();
 };
