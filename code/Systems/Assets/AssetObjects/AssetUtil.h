14f324aa91777de5b5be6386de0de881b5668992
Florent Deville <florent.devillechabrol@gmail.com>
2026-03-31 10:57:15 -0700

    [sys] AssetUtil : move template functions to inl file.

--- code/Systems/Assets/AssetObjects/AssetUtil.h
+++ code/Systems/Assets/AssetObjects/AssetUtil.h
@@ -31,55 +31,6 @@ namespace Systems
 
 		template<class T> static bool IsA(NewAssetId id);
 	};
-
-	template<class T> T* AssetUtil::GetAsset(NewAssetId id, LoadingDomain domain)
-	{
-		return static_cast<T*>(GetAsset(id, domain));
-	}
-
-	template<class T> T* AssetUtil::LoadAsset(NewAssetId id, LoadingDomain domain)
-	{
-		return static_cast<T*>(LoadAsset(id, domain));
-	}
-
-	template<class T> T* AssetUtil::CreateAsset(const std::string& virtualName)
-	{
-		T* pAsset = Systems::CreateNewAsset<T>();
-		if (!pAsset)
-			return nullptr;
-
-		AssetMgr& assetMgr = AssetMgr::Get();
-		ContainerId cid = assetMgr.GenerateNewContainerId();
-
-		ContainerMgr& containerMgr = ContainerMgr::Get();
-		Container* pContainer = containerMgr.CreateContainer(cid);
-		pContainer->AddAsset(pAsset);
-		bool res = containerMgr.SaveContainer(pContainer->GetId());
-		if (!res)
-			return nullptr;
-
-		AssetMetadata materialMetadata(pAsset->GetId(), virtualName, T::GetAssetTypeNameSid());
-		
-		res = assetMgr.RegisterAssetMetadata(materialMetadata);
-		if (!res)
-			return nullptr;
-
-		res = assetMgr.SaveMetadataTable();
-		if (!res)
-			return nullptr;
-
-		return pAsset;
-	}
-
-	template<class T> bool AssetUtil::IsA(NewAssetId id)
-	{
-		const Systems::AssetMetadata* pMetadata = Systems::AssetMgr::Get().GetMetadata(id);
-		if (!pMetadata)
-			return false;
-
-		if (pMetadata->GetAssetType() == T::GetAssetTypeNameSid())
-			return true;
-
-		return false;
-	}
 }
+
+#include "Systems/Assets/AssetObjects/AssetUtil.inl"
