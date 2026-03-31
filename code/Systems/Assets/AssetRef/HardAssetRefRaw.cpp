7ceb4af77220b974f190ecbc22528d9dcb359bda
Florent Deville <florent.devillechabrol@gmail.com>
2026-03-31 13:53:44 -0700

    Reapply "[sys] HardAssetRefRaw : unload asset in destructor."
    This reverts commit add940f55fe3d76c9bef6243d9a2ad60a3f80cc0.

--- code/Systems/Assets/AssetRef/HardAssetRefRaw.cpp
+++ code/Systems/Assets/AssetRef/HardAssetRefRaw.cpp
@@ -11,21 +11,25 @@ namespace Systems
 	HardAssetRefRaw::HardAssetRefRaw()
 		: m_id()
 		, m_pPtr(nullptr)
+		, m_domain(LoadingDomain::UNKNOWN)
 	{ }
 
 	HardAssetRefRaw::HardAssetRefRaw(NewAssetId id)
 		: m_id(id)
 		, m_pPtr(nullptr)
+		, m_domain(LoadingDomain::UNKNOWN)
 	{ }
 
 	HardAssetRefRaw::~HardAssetRefRaw()
 	{
+		AssetUtil::UnloadAsset(m_id, m_domain);
 		m_id = NewAssetId::INVALID;
 		m_pPtr = nullptr;
 	}
 
 	bool HardAssetRefRaw::Resolve(LoadingDomain domain)
 	{
+		m_domain = domain;
 		m_pPtr = AssetUtil::GetAsset(m_id, domain);
 		return m_pPtr != nullptr;
 	}
@@ -42,6 +46,7 @@ namespace Systems
 
 	bool HardAssetRefRaw::Load(LoadingDomain domain)
 	{
+		m_domain = domain;
 		m_pPtr = AssetUtil::LoadAsset(m_id, domain);
 		return IsResolved();
 	}
