49c56dcfb73110562159266596b26dc2af94288e
Florent Deville <florent.devillechabrol@gmail.com>
2026-04-01 14:52:00 -0700

    [alpha] PlayerComponent : move character using input command instead of virtual keys.

--- code/Alpha/Components/PlayerComponent.cpp
+++ code/Alpha/Components/PlayerComponent.cpp
@@ -8,8 +8,6 @@
 
 #include "Inputs/InputMgr.h"
 
-#include "OsWin/VirtualKeyCode.h"
-
 #include "Rendering/Camera.h"
 
 #include "Systems/Game/GameMgr.h"
@@ -53,14 +51,14 @@ void PlayerComponent::Update(float dt)
 	Inputs::InputMgr& inputMgr = Inputs::InputMgr::Get();
 
 	Core::Vec4f direction;
-	if (inputMgr.IsKeyPressed(Os::VKeyCodes::Vk_W))
+	if (inputMgr.GetState(Inputs::InputCommand::MoveForward))
 		direction = direction + forward;
-	else if(inputMgr.IsKeyPressed(Os::VKeyCodes::Vk_S))
+	else if(inputMgr.GetState(Inputs::InputCommand::MoveBackward))
 		direction = direction - forward;
 
-	if (inputMgr.IsKeyPressed(Os::VKeyCodes::Vk_A))
+	if (inputMgr.GetState(Inputs::InputCommand::MoveLeft))
 		direction = direction - right;
-	else if (inputMgr.IsKeyPressed(Os::VKeyCodes::Vk_D))
+	else if (inputMgr.GetState(Inputs::InputCommand::MoveRight))
 		direction = direction + right;
 
 	if (direction.Length() != 0)
