7d54950f9a036cf856033e4fde1cd0534c9b1f4c
Florent Deville <florent.devillechabrol@gmail.com>
2026-04-03 11:53:35 -0700

    [alpha] Bloom downsample : fix sampler parameters.

--- code/Alpha/Shaders/bloom/bloom_downsample.rs.hlsl
+++ code/Alpha/Shaders/bloom/bloom_downsample.rs.hlsl
@@ -10,7 +10,10 @@
 					"DENY_DOMAIN_SHADER_ROOT_ACCESS |" \
 					"DENY_GEOMETRY_SHADER_ROOT_ACCESS)," \
 					"RootConstants(num32BitConstants=2, b0, space=0, visibility=SHADER_VISIBILITY_PIXEL)," \
-					"StaticSampler(s0, visibility=SHADER_VISIBILITY_PIXEL), " \
+					"StaticSampler(s0, filter=FILTER_MIN_MAG_MIP_LINEAR, addressU=TEXTURE_ADDRESS_CLAMP, "\
+										"addressV=TEXTURE_ADDRESS_CLAMP, "\
+										"addressW=TEXTURE_ADDRESS_CLAMP,"\
+										"visibility=SHADER_VISIBILITY_PIXEL), " \
 					"DescriptorTable(SRV(t0, space=0))"
 
 #endif
