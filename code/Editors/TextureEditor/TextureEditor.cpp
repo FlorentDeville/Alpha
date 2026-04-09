07dbe4f32550287c49b67de296a7f8c031177c8e
Florent Deville <florent.devillechabrol@gmail.com>
2026-04-09 09:59:53 -0700

    [edt] TextureEditor : add timer for cubemap import.

--- code/Editors/TextureEditor/TextureEditor.cpp
+++ code/Editors/TextureEditor/TextureEditor.cpp
@@ -287,7 +287,20 @@ namespace Editors
 		if (!Systems::AssetUtil::IsA<Systems::CubemapAsset>(id))
 			return;
 
-		TextureEditorModule::Get().ImportTexture(id);
+		std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
+
+		bool res = TextureEditorModule::Get().ImportTexture(id);
+
+		std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
+		std::chrono::duration elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+
+		Systems::AssetMetadata* pMetadata = Systems::AssetMgr::Get().GetMetadata(id);
+		assert(pMetadata); //if I don;t have a metadata here, something really wrong is going on.
+
+		if (!res)
+			Core::LogModule::Get().LogError("Failed to import cubemap %s in %f s.", pMetadata->GetVirtualName().c_str(), elapsedTime.count() / 1000.f);
+		else
+			Core::LogModule::Get().LogInfo("Cubemap %s imported in %f s.", pMetadata->GetVirtualName().c_str(), elapsedTime.count() / 1000.f);
 	}
 
 	void TextureEditor::OnClick_File_Save()
