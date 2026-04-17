e8aa113104b76d7c6586904c2bf4e04837d3d07f
Florent Deville <florent.devillechabrol@gmail.com>
2026-04-16 21:37:59 -0700

    [hlsl] particle vertex shader : fix billboard.

--- code/Alpha/Shaders/Particles/particles.vs.hlsl
+++ code/Alpha/Shaders/Particles/particles.vs.hlsl
@@ -41,10 +41,15 @@ PSInput main(uint id : SV_VertexID, uint instanceID : SV_InstanceID)
 {
     PSInput output;
     
-    float4 viewLocation = FrameParameter.m_view[3];
-
     //render a single quad using the vertex id and awlays facing the camera.
-    output.position = pos[id] + ParticlePosition[instanceID].m_pos + viewLocation;
+    
+    //first transform the quad center to the view space
+    output.position = mul(ParticlePosition[instanceID].m_pos, FrameParameter.m_view);
+    
+    //in the view space, add the offset of the vertex
+    output.position = output.position + pos[id];
+    
+    //now project it
     output.position = mul(output.position, FrameParameter.m_proj);
     
     output.uv = uv[id];
