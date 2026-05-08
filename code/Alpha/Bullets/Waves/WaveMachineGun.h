d0a822bb24d2877b02aa35b95171ce8d0d1064ae
Florent Deville <florent.devillechabrol@gmail.com>
2026-05-08 09:39:41 -0700

    [alpha] Add new wave : machine gun.

---
+++ code/Alpha/Bullets/Waves/WaveMachineGun.h
@@ -0,0 +1,33 @@
+/********************************************************************************/
+/* Copyright (C) 2026 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************************/
+
+#pragma once
+
+#include "Alpha/Bullets/IBulletWave.h"
+
+namespace Systems
+{
+	class GameObject;
+}
+
+class WaveMachineGun : public IBulletWave
+{
+public:
+	WaveMachineGun(Systems::MeshAsset* pMesh, Systems::MaterialInstanceAsset* pMaterial, const Systems::GameObject* pOwner, const Systems::GameObject* pTarget);
+	~WaveMachineGun();
+
+	void Init(Bullets& bullets) override;
+	void Destroy(Bullets& bullets) override;
+	void Start(Bullets& bullets, const Core::Vec4f& pos) override;
+	void Stop() override;
+	void Update(Bullets& bullets, float dt) override;
+	void BuildRenderable(Bullets& bullets, Systems::RenderableScene& scene) override;
+
+private:
+	const Systems::GameObject* m_pOwner;
+	const Systems::GameObject* m_pTarget;
+
+	uint32_t m_nextBulletToShot;
+	float m_lastSpawnTime;
+};
