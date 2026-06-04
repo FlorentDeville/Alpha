0173b9f761057c12a84e53620d2f315fdf53441f
Florent Deville <florent.devillechabrol@gmail.com>
2026-06-04 10:04:33 -0700

    [Alpha] Add BaseBoss game object.

---
+++ code/Alpha/Objects/Boss/BaseBoss.cpp
@@ -0,0 +1,57 @@
+/********************************************************************************/
+/* Copyright (C) 2026 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************************/
+
+#include "Alpha/Objects/Boss/BaseBoss.h"
+
+#include "Alpha/StateMachine/StateMachine.h"
+
+BaseBoss::BaseBoss()
+	: BaseClass()
+	, m_maxHP(100)
+	, m_currentHP(100)
+	, m_pStateMachine(nullptr)
+{ }
+
+BaseBoss::~BaseBoss()
+{
+	delete m_pStateMachine;
+	m_pStateMachine = nullptr;
+}
+
+void BaseBoss::OnStartGame()
+{
+	BaseClass::OnStartGame();
+
+	m_currentHP = m_maxHP;
+}
+
+void BaseBoss::Update(float dt)
+{
+	BaseClass::Update(dt);
+}
+
+void BaseBoss::HandleMessage(const Systems::GameMessage& msg)
+{
+	BaseClass::HandleMessage(msg);
+}
+
+void BaseBoss::OnDestroyGame()
+{
+	BaseClass::OnDestroyGame();
+}
+
+uint32_t BaseBoss::GetMaxHP() const
+{
+	return m_maxHP;
+}
+
+int32_t BaseBoss::GetCurrentHP() const
+{
+	return m_currentHP;
+}
+
+void BaseBoss::SetCurrentHP(int32_t hp)
+{
+	m_maxHP = hp;
+}
---
+++ code/Alpha/Objects/Boss/BaseBoss.h
@@ -0,0 +1,58 @@
+/********************************************************************************/
+/* Copyright (C) 2026 Florent Devillechabrol <florent.devillechabrol@gmail.com>	*/
+/********************************************************************************/
+
+#pragma once
+
+#include "Core/Reflection/ReflectionMacro.h"
+
+//#include "Systems/Assets/AssetObjects/MaterialInstance/MaterialInstanceAsset.h"
+//#include "Systems/Assets/AssetObjects/Mesh/MeshAsset.h"
+//#include "Systems/GameComponent/Collisions/CollisionSphereComponent.h"
+//#include "Systems/GameComponent/ComponentRef/ComponentRef.h"
+//#include "Systems/GameComponent/UI/UIBaseComponent.h"
+#include "Systems/Objects/GameObject.h"
+
+class StateMachine;
+
+ENABLE_GAME_REFLECTION(BaseBoss)
+
+class BossState_Phase1_Attack;
+class BossState_Phase2_Attack1;
+class BossState_Phase2_Attack2;
+class BossState_Phase3_Attack1;
+class BossState_Phase3_Attack2;
+
+class BaseBoss : public Systems::GameObject
+{
+	using BaseClass = Systems::GameObject;
+
+public:
+	BaseBoss();
+	~BaseBoss();
+
+	void OnStartGame() override;
+
+	void Update(float dt) override;
+
+	void HandleMessage(const Systems::GameMessage& msg) override;
+
+	void OnDestroyGame() override;
+
+	uint32_t GetMaxHP() const;
+	int32_t GetCurrentHP() const;
+
+	void SetCurrentHP(int32_t hp);
+
+protected:
+	uint32_t m_maxHP;
+
+	START_REFLECTION(BaseBoss)
+		ADD_BASETYPE(Systems::GameObject)
+		ADD_FIELD(m_maxHP)
+	END_REFLECTION()
+
+protected:
+	StateMachine* m_pStateMachine;
+	int32_t m_currentHP;
+};
