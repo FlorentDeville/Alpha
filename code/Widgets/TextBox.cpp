4f1ab073c7ffffc7c8cf7fbcb4c730023c5b4d75
Florent Deville <florent.devillechabrol@gmail.com>
2026-06-13 14:29:20 -0700

    [widgets] TextBox : don't display the text when it's above the scissor.

--- code/Widgets/TextBox.cpp
+++ code/Widgets/TextBox.cpp
@@ -377,6 +377,9 @@ namespace Widgets
 		DirectX::XMFLOAT3 uiPos((float)m_absPos.x + m_textLocalPos.x, (float)m_absPos.y + m_textLocalPos.y, (float)m_absPos.z - 1);
 		DirectX::XMUINT4 localScissor(m_absPos.x, m_absPos.y, m_size.x, m_size.y);
 
+		if (localScissor.y < scissor.top)
+			return;
+
 		int32_t bottom = localScissor.y + localScissor.w;
 		if (bottom <= scissor.bottom)
 			Rendering::RenderModule::Get().PrepareRenderText(m_text, WidgetMgr::Get().GetUIFontId(), uiPos, DirectX::XMFLOAT2(fontScale, fontScale), localScissor, Widget::NEAR_CAMERA_PLANE, Widget::FAR_CAMERA_PLANE);
